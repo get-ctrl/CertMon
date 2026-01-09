fff9b13614c86d57d724ad5cb867fdc2d0699128
get-ctrl <noah.nosraep@gmail.com>
2026-01-09 11:49:15 -0600

    Update certmon.py

--- certmon.py
+++ certmon.py
@@ -1,10 +1,14 @@
 import asyncio
 import psycopg
 import ssl
+import sys
 
-certsh_conn_str = "postgresql://guest@crt.sh:5432/certwatch"
-sql_query_last = "SELECT * FROM certificate c ORDER BY c.ID DESC LIMIT 8000"
-sql_query_since = "SELECT * FROM certificate c WHERE c.ID > {} ORDER BY c.ID DESC LIMIT 8000"
+CONN_STR_CRTSH = "postgresql://guest@crt.sh:5432/certwatch"
+QUERY_STR_LAST = "SELECT * FROM certificate c ORDER BY c.ID DESC LIMIT 1000"
+QUERY_STR_SINCE = "SELECT * FROM certificate c WHERE c.ID > {} ORDER BY c.ID ASC LIMIT 1000"
+
+if sys.platform == 'win32':
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 class CertMon:
     
@@ -15,33 +19,48 @@ class CertMon:
         self.interval = 0.01
     
     async def connect(self):
-        self.client = psycopg.connect(certsh_conn_str, autocommit=True, prepare_threshhold=None)
-        self.cur = self.client.cursor()
+        self.conn = await psycopg.AsyncConnection.connect(CONN_STR_CRTSH, autocommit=True, prepare_threshold=None)
+        self.cur = self.conn.cursor()
     
     async def disconnect(self):
-        self.client.close()
+        if self.conn: self.conn.close()
+    
+    async def reconnect(self):
+        await self.disconnect()
+        await asyncio.sleep(5)
+        await self.connect()
     
     async def query(self, query_str):
-        self.cur.execute(query_str)
-        results = self.cur.fetchall()
+        await self.cur.execute(query_str)
+        return await self.cur.fetchall()
+    
+    async def query_latest(self):
+        results = await self.query(QUERY_STR_LAST)
+        results.reverse()
+        await self.dispatch(results)
+    
+    async def query_since(self):
+        query_str = QUERY_STR_SINCE.format(self.lastid)
+        results = await self.query(query_str)
+        await self.dispatch(results)
+    
+    async def dispatch(self, results):
         for entry in results:
-            self.lastid = entry[0]
             await self.callback(entry)
+            entry_id = entry[0]
+            self.lastid = entry_id
             await asyncio.sleep(self.interval)
     
     async def start(self):
         self.running = True
-        await self.query(sql_query_last)
+        await self.query_latest()
         while self.running:
             try:
-                query_str = sql_query_since.format(self.lastid)
-                await self.query(query_str)
+                await self.query_since()
             except Exception as ex:
                 print(ex)
-                await self.disconnect()
-                await asyncio.sleep(5)
-                await self.connect()
+                await self.reconnect()
         
     async def stop(self):
-        await self.disconnect()
         self.running = False
+        await self.disconnect()
