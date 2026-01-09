d08d71f15f58f0c68cfc6a63f493facde883d53e
get-ctrl <noah.nosraep@gmail.com>
2026-01-08 22:12:29 -0600

    Update certmon.py

--- certmon.py
+++ certmon.py
@@ -3,52 +3,45 @@ import psycopg
 import ssl
 
 certsh_conn_str = "postgresql://guest@crt.sh:5432/certwatch"
-sql_query_last = "SELECT * FROM certificate ORDER BY id DESC LIMIT 1000"
-sql_query_since = "SELECT * FROM certificate c WHERE c.ID > {} ORDER BY c.ID DESC LIMIT 1000"
+sql_query_last = "SELECT * FROM certificate c ORDER BY c.ID DESC LIMIT 8000"
+sql_query_since = "SELECT * FROM certificate c WHERE c.ID > {} ORDER BY c.ID DESC LIMIT 8000"
 
 class CertMon:
     
     def __init__(self, callback):
         self.callback = callback
         self.running = False
-        self.interval = 0.01
         self.lastid = -1
+        self.interval = 0.01
     
     async def connect(self):
-        self.client = psycopg.connect(certsh_conn_str, autocommit=True)
+        self.client = psycopg.connect(certsh_conn_str, autocommit=True, prepare_threshhold=None)
+        self.cur = self.client.cursor()
     
-    async def run(self):
-        self.running = True
-        cur = self.client.cursor()
-        
-        cur.execute(sql_query_last)
-        results = cur.fetchall()
-        await self.dispatch_messages(results)
-        
-        while self.running:
-            try:
-                if self.lastid == -1:
-                    raise Exception("Last id was not set on initial query")
-                
-                query = sql_query_since.format(self.lastid)
-                cur.execute(query)
-                results = cur.fetchall()
-                
-                await self.dispatch_messages(results)
-            except Exception as ex:
-                print(ex)
-                await asyncio.sleep(10)
-        
-    async def stop(self):
-        self.running = False
+    async def disconnect(self):
+        self.client.close()
     
-    async def dispatch_messages(self, results):
-        self.lastid = results[0][0]
-        for result in results:
-            self.dispatch_message(result)
+    async def query(self, query_str):
+        self.cur.execute(query_str)
+        results = self.cur.fetchall()
+        for entry in results:
+            self.lastid = entry[0]
+            await self.callback(entry)
             await asyncio.sleep(self.interval)
     
-    def dispatch_message(self, entry):
-        if not entry: return
-        self.lastid = entry[0]
-        self.callback(entry)
+    async def start(self):
+        self.running = True
+        await self.query(sql_query_last)
+        while self.running:
+            try:
+                query_str = sql_query_since.format(self.lastid)
+                await self.query(query_str)
+            except Exception as ex:
+                print(ex)
+                await self.disconnect()
+                await asyncio.sleep(5)
+                await self.connect()
+        
+    async def stop(self):
+        await self.disconnect()
+        self.running = False
