32e979772ad90939ed96b9e0b1dfe183a0f63b0a
get-ctrl <noah.nosraep@gmail.com>
2026-01-13 19:18:52 -0600

    Update certscan.py

--- examples/certscan.py
+++ examples/certscan.py
@@ -3,28 +3,18 @@ from cryptography import x509
 from certmon import CertMon
 
 FLAG_CN = x509.oid.NameOID.COMMON_NAME
-FLAG_SAN = x509.SubjectAlternativeName
-FLAG_DNS = x509.DNSName
 
 def x509_decode(cert_binary):
     return x509.load_der_x509_certificate(cert_binary)
 
-def x509_get_subjectNames(cert):
-    domains = []
-    try:
-        cn = cert.subject.get_attributes_for_oid(FLAG_CN)
-        san = cert.extensions.get_extension_for_class(FLAG_SAN)
-        if cn: domains.append(cn[0].value)
-        for name in san.value.get_values_for_type(FLAG_DNS):
-            domains.append(name)
-    except:
-        pass
-    return domains
+def x509_getdomain(cert):
+    cn = cert.subject.get_attributes_for_oid(FLAG_CN)
+    return cn[0].value
 
 async def callback(entry):
-    cert = x509_decode(entry[2])
-    domains = x509_get_subjectNames(cert)
-    print(str(entry[0]) + " " + ", ".join(domains))
+    cert = x509_decode(entry.binary)
+    domain = x509_getdomain(cert)
+    print(entry.id, domain)
 
 async def main():
     mon = CertMon(callback)
